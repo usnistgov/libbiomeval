a4e0ebe06005c5c16131439ca0fb6e447ef2b890
Greg Fiumara <gregory.fiumara@nist.gov>
2010-08-02 15:03:45 -0500

    Include proper licensing and header guard information for Timer class.
    [git-p4: depot-paths = "//depot/projects/EVALUATION/Main/common/": change = 1867]

--- src/include/be_timer.h
+++ src/include/be_timer.h
@@ -1,3 +1,16 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology (NIST) by employees of the Federal Government in the course
+ * of their official duties. Pursuant to title 17 Section 105 of the
+ * United States Code, this software is not subject to copyright protection
+ * and is in the public domain. NIST assumes no responsibility whatsoever for
+ * its use by other parties, and makes no guarantees, expressed or implied,
+ * about its quality, reliability, or any other characteristic.
+ */
+
+#ifndef __BE_TIMER_H__
+#define __BE_TIMER_H__
+
 #include <be_exception.h>
 
 namespace BiometricEvaluation 
@@ -45,3 +58,6 @@ namespace BiometricEvaluation
 		};
 	};
 }
+
+#endif /* __BE_TIMER_H__ */
+
