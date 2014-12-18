eaeb236218ca43226fdb3d0aba247de20770b8b8
Wayne Salamon <wsalamon@nist.gov>
2014-12-18 10:38:05 -0500

    Add some notes to the MPI test program script regarding logsheet URLs, and the use of rsyslog as a logging facility.
    [git-p4: depot-paths = "//depot/projects/EVALUATION/Main/common/": change = 6300]

--- src/test/mpitest.sh
+++ src/test/mpitest.sh
@@ -13,6 +13,23 @@ INPUTRS=test_data/AN2KRecordStore
 #
 # Create the properties file for this run
 #
+# Logsheet URL is used by the framework for logging and is optional.
+# Record Logsheet URL is defined and used by the application and is
+# optional in the test_be_mpi program.
+#
+# An example config file for rsyslogd, listening on a non-default port:
+#
+#       $ModLoad imtcp
+#       # Provides TCP syslog reception
+#       $InputTCPServerRun 2514
+#       local0.info /home/wsalamon/sandbox/rsyslog/record.log
+#       local1.debug /home/wsalamon/sandbox/rsyslog/debug.log
+#
+# Example of starting rsyslogd:
+#	rsyslogd -i $PWD/rsyslog.pid -f rsyslog.conf
+#
+# NOTE: rsyslogd requires full path names for files.
+#
 PROPS=test_be_mpi.props
 cat > $PROPS << EOF
 Input Record Store = $INPUTRS
@@ -20,6 +37,8 @@ Chunk Size = 4
 Workers Per Node = 2
 Logsheet URL = file://mpi.log
 Record Logsheet URL = file://record.log
+#Logsheet URL = syslog://linc01b:2514
+#Record Logsheet URL = syslog://linc01b:2514
 EOF
 
 #
