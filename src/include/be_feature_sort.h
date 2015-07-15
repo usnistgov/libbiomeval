7fa313c3b6e76f47f8cbb1f6b1a4aa270a985362
Greg Fiumara <gregory.fiumara@nist.gov>
2015-07-15 14:05:23 -0500

    Add MinutiaPointSet sort comparators and enumeration from MINEX III to framework.
    [git-p4: depot-paths = "//depot/projects/EVALUATION/Main/common/": change = 7003]

---
+++ src/include/be_feature_sort.h
@@ -0,0 +1,253 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology (NIST) by employees of the Federal Government in the course
+ * of their official duties.  Pursuant to title 17 Section 105 of the
+ * United States Code, this software is not subject to copyright protection
+ * and is in the public domain.  NIST assumes no responsibility whatsoever for
+ * its use by other parties, and makes no guarantees, expressed or implied,
+ * about its quality, reliability, or any other characteristic.
+ */
+
+#ifndef BE_FEATURE_SORT_H_
+#define BE_FEATURE_SORT_H_
+
+#include <be_feature_minutiae.h>
+#include <be_image.h>
+
+namespace BiometricEvaluation
+{
+	namespace Feature
+	{
+		/** Utilities for sorting MinutiaPointSets. */
+		namespace Sort
+		{
+			/** Sort order of MinutiaPointSets. */
+			enum class Kind
+			{
+				/**
+				 * @brief
+				 * Lowest to highest X value, followed by Y
+				 * value.
+				 */
+				XYAscending,
+				/**
+				 * @brief
+				 * Highest to lowest X value, followed by Y
+				 * value.
+				 */
+				XYDescending,
+				/**
+				 * @brief
+				 * Lowest to highest Y value, followed by X
+				 * value.
+				 */
+				YXAscending,
+				/**
+				 * @brief 
+				 * Highest to lowest Y value, followed by X
+				 * value.
+				 */
+				YXDescending,
+				/** Lowest to highest quality value. */
+				QualityAscending,
+				/** Highest to lowest quality value. */
+				QualityDescending,
+				/** Lowest to highest angle (theta) value. */
+				AngleAscending,
+				/** Highest to lowest angle (theta) value. */
+				AngleDescending,
+				/**
+				 * @brief
+				 * Lowest to highest distance from center of
+				 * minutia mass, followed by angle (theta).
+				 */
+				PolarCOMAscending,
+				/**
+				 * @brief
+				 * Highest to lowest distance from center of
+				 * minutia mass, followed by angle (theta).
+				 */
+				PolarCOMDescending,
+				/**
+				 * @brief
+				 * Lowest to highest distance from center of
+				 * image, followed by angle (theta).
+				 */
+				PolarCOIAscending,
+				/**
+				 * @brief
+				 * Highest to lowest distance from center of
+				 * img, followed by angle (theta).
+				 */
+				PolarCOIDescending,
+				/** Sort order cannot be determined. */
+				Unknown
+			};
+
+			/** Sort by increasing Cartesian X-Y coordinate */
+			class XY
+			{
+			public:
+				/**
+				 * @brief
+				 * MinutiaPoint Cartesian X-Y ascending
+				 * comparator.
+				 */
+				bool
+				operator()(
+				    const BiometricEvaluation::Feature::
+				    MinutiaPoint &lhs,
+				    const BiometricEvaluation::Feature::
+				    MinutiaPoint &rhs)
+				    const;
+			};
+		
+			/** Sort by increasing Cartesian Y-X coordinate */
+			class YX
+			{
+			public:
+				/** 
+				 * @brief
+				 * MinutiaPoint Cartesian Y-X ascending
+				 * comparator.
+				 */
+				bool
+				operator()(
+				    const BiometricEvaluation::Feature::
+				    MinutiaPoint &lhs,
+				    const BiometricEvaluation::Feature::
+				    MinutiaPoint &rhs)
+				    const;
+			};
+
+			/** Sort by increasing minutiae quality */
+			class Quality
+			{
+			public:
+				/**
+				 * @brief
+				 * MinutiaPoint quality ascending comparator.
+				 */
+				bool
+				operator()(
+				    const BiometricEvaluation::Feature::
+				    MinutiaPoint &lhs,
+				    const BiometricEvaluation::Feature::
+				    MinutiaPoint &rhs)
+				    const;
+			};
+		
+			/** Sort by increasing angle (theta) */
+			class Angle
+			{
+			public:
+				/** MinutiaPoint angle ascending comparator. */
+				bool
+				operator()(
+				    const BiometricEvaluation::Feature::
+				    MinutiaPoint &lhs,
+				    const BiometricEvaluation::Feature::
+				    MinutiaPoint &rhs)
+				    const;
+			};
+		
+			/**
+			 * @brief
+			 * Sort by increasing distance from center and angle
+			 * (theta).
+			 */
+			class Polar
+			{
+			public:
+				/**
+				 * @brief
+				 * Polar constructor.
+				 *
+				 * @param center
+				 * Coordinate to use for center of image.
+				 *
+				 * @seealso centerOfMinutiaeMass
+				 * @seealso centerOfImage
+				 */
+				Polar(
+				    const BiometricEvaluation::Image::Coordinate
+				    &center);
+			
+				/** MinutiaPoint polar ascending comparator. */
+				bool
+				operator()(
+				    const BiometricEvaluation::Feature::
+				    MinutiaPoint &lhs,
+				    const BiometricEvaluation::Feature::
+				    MinutiaPoint &rhs)
+				    const;
+			
+				/**
+				 * @brief
+				 * Obtain the center of minutiae mass.
+				 *
+				 * @param mps
+				 * Collection of minutia points.
+				 *
+				 * @return
+				 * Center of minutiae mass for mps.
+				 */
+				static BiometricEvaluation::Image::Coordinate
+				centerOfMinutiaeMass(
+				    const BiometricEvaluation::Feature::
+				    MinutiaPointSet &mps);
+			
+				/**
+				 * @brief
+				 * Obtain the center point of an image.
+				 *
+				 * @param size
+				 * Size of an image.
+				 *
+				 * @note
+				 * If dimensions are odd, integer division is
+				 * applied.
+				 */
+				static BiometricEvaluation::Image::Coordinate
+				centerOfImage(
+				    const BiometricEvaluation::Image::Size
+				    &size);
+			
+			private:
+				/** Center coordinate. */
+				BiometricEvaluation::Image::Coordinate _center;
+			
+				/**
+				 * @brief
+				 * Obtain a value indicative of distance from
+				 * _center with 0 being equivalent to center.a
+				 *
+				 * @param coordinate
+				 * Coordinate to compare to _center.
+				 *
+				 * @return
+				 * A value indicative of distance from _center.
+				 *
+				 * @note
+				 * Not the "true" distance.
+				 */
+				uint64_t
+				distanceFromCenter(
+				    const BiometricEvaluation::Image::Coordinate
+				    &coordinate)
+				    const;
+			};
+		
+			/**
+			 * @brief
+			 * Renumber index numbers in a MinutiaPointSet in
+			 * place.
+			 */
+			void
+			updateIndicies(
+			    BiometricEvaluation::Feature::MinutiaPointSet &mps);
+		}
+	}
+}
+
+#endif /* BE_FEATURE_SORT_H_ */
---
+++ src/libbiomeval/be_feature_sort.cpp
@@ -0,0 +1,183 @@
+/*
+ * This software was developed at the National Institute of Standards and
+ * Technology (NIST) by employees of the Federal Government in the course
+ * of their official duties.  Pursuant to title 17 Section 105 of the
+ * United States Code, this software is not subject to copyright protection
+ * and is in the public domain.  NIST assumes no responsibility whatsoever for
+ * its use by other parties, and makes no guarantees, expressed or implied,
+ * about its quality, reliability, or any other characteristic.
+ */
+
+#include <be_feature_sort.h>
+#include <be_framework_enumeration.h>
+
+
+
+bool
+BiometricEvaluation::Feature::Sort::XY::operator()(
+    const BiometricEvaluation::Feature::MinutiaPoint &lhs,
+    const BiometricEvaluation::Feature::MinutiaPoint &rhs)
+    const
+{
+	/* Ascending X */
+	if (lhs.coordinate.x > rhs.coordinate.x)
+		return (false);
+	if (lhs.coordinate.x < rhs.coordinate.x)
+		return (true);
+
+	/* Ascending Y */
+	if (lhs.coordinate.y > rhs.coordinate.y)
+		return (false);
+	if (lhs.coordinate.y < rhs.coordinate.y)
+		return (true);
+
+	/* Equivalent */
+	return (false);
+}
+
+bool
+BiometricEvaluation::Feature::Sort::YX::operator()(
+    const BiometricEvaluation::Feature::MinutiaPoint &lhs,
+    const BiometricEvaluation::Feature::MinutiaPoint &rhs)
+    const
+{
+	/* Ascending Y */
+	if (lhs.coordinate.y > rhs.coordinate.y)
+		return (false);
+	if (lhs.coordinate.y < rhs.coordinate.y)
+		return (true);
+
+	/* Ascending X */
+	if (lhs.coordinate.x > rhs.coordinate.x)
+		return (false);
+	if (lhs.coordinate.x < rhs.coordinate.x)
+		return (true);
+
+	/* Equivalent */
+	return (false);
+}
+
+bool
+BiometricEvaluation::Feature::Sort::Quality::operator()(
+    const BiometricEvaluation::Feature::MinutiaPoint &lhs,
+    const BiometricEvaluation::Feature::MinutiaPoint &rhs)
+    const
+{
+	if (lhs.has_quality && rhs.has_quality)
+		return (lhs.quality < rhs.quality);
+	return (false);
+}
+
+bool
+BiometricEvaluation::Feature::Sort::Angle::operator()(
+    const BiometricEvaluation::Feature::MinutiaPoint &lhs,
+    const BiometricEvaluation::Feature::MinutiaPoint &rhs)
+    const
+{
+	return (lhs.theta < rhs.theta);
+}
+
+BiometricEvaluation::Feature::Sort::Polar::Polar(
+    const BiometricEvaluation::Image::Coordinate &center) :
+    _center(center)
+{
+
+}
+
+uint64_t
+BiometricEvaluation::Feature::Sort::Polar::distanceFromCenter(
+    const BiometricEvaluation::Image::Coordinate &coordinate)
+    const
+{
+	/*
+	 * We don't care about the actual distance value, so don't use
+	 * BiometricEvaluation::Image::distance() (to avoid floating point,
+	 * for speed)). 
+	 */
+	const int64_t xDelta = static_cast<int64_t>(coordinate.x) - 
+	    static_cast<int64_t>(this->_center.x);
+	const int64_t yDelta = static_cast<int64_t>(coordinate.y) - 
+	    static_cast<int64_t>(this->_center.y);
+	return ((xDelta * xDelta) + (yDelta * yDelta));
+}
+
+BiometricEvaluation::Image::Coordinate
+BiometricEvaluation::Feature::Sort::Polar::centerOfMinutiaeMass(
+    const BiometricEvaluation::Feature::MinutiaPointSet &mps)
+{
+	uint32_t sumX = 0, sumY = 0;
+	for (const auto &minutiae : mps) {
+		sumX += minutiae.coordinate.x;
+		sumY += minutiae.coordinate.y;
+	}
+	
+	const uint32_t count = mps.size();
+	return {(sumX / count), (sumY / count)};
+}
+
+BiometricEvaluation::Image::Coordinate
+BiometricEvaluation::Feature::Sort::Polar::centerOfImage(
+    const BiometricEvaluation::Image::Size &size)
+{
+	return {(size.xSize / 2), (size.ySize / 2)};
+}
+
+bool
+BiometricEvaluation::Feature::Sort::Polar::operator()(
+    const BiometricEvaluation::Feature::MinutiaPoint &lhs,
+    const BiometricEvaluation::Feature::MinutiaPoint &rhs)
+    const
+{
+	const uint64_t lhsDistance = Polar::distanceFromCenter(lhs.coordinate);
+	const uint64_t rhsDistance = Polar::distanceFromCenter(rhs.coordinate);
+
+	/* Increasing distance from center */
+	if (lhsDistance > rhsDistance)
+		return (false);
+	if (lhsDistance < rhsDistance)
+		return (true);
+	
+	/* Increasing theta */
+	if (lhs.theta > rhs.theta)
+		return (false);
+	if (lhs.theta < rhs.theta)
+		return (true);
+	
+	return (false);
+}
+
+void
+BiometricEvaluation::Feature::Sort::updateIndicies(
+    BiometricEvaluation::Feature::MinutiaPointSet &mps)
+{
+	for (uint64_t i = 0; i < mps.size(); i++)
+		mps[i].index = i;
+}
+
+template<>
+const std::map<BiometricEvaluation::Feature::Sort::Kind, std::string>
+BiometricEvaluation::Framework::EnumerationFunctions<
+    BiometricEvaluation::Feature::Sort::Kind>::enumToStringMap {
+	{BiometricEvaluation::Feature::Sort::Kind::XYAscending, "XY Ascending"},
+	{BiometricEvaluation::Feature::Sort::Kind::XYDescending,
+	    "XY Descending"},
+	{BiometricEvaluation::Feature::Sort::Kind::YXAscending, "YX Ascending"},
+	{BiometricEvaluation::Feature::Sort::Kind::YXDescending,
+	    "YX Descending"},
+	{BiometricEvaluation::Feature::Sort::Kind::QualityAscending,
+	    "Quality Ascending"},
+	{BiometricEvaluation::Feature::Sort::Kind::QualityDescending,
+	    "Quality Descending"},
+	{BiometricEvaluation::Feature::Sort::Kind::AngleAscending,
+	    "Angle Ascending"},
+	{BiometricEvaluation::Feature::Sort::Kind::AngleDescending,
+	    "Angle Descending"},
+	{BiometricEvaluation::Feature::Sort::Kind::PolarCOMAscending,
+	    "Polar Center of Minutia Mass Ascending"},
+	{BiometricEvaluation::Feature::Sort::Kind::PolarCOMDescending,
+	    "Angle Center of Minutia Mass Descending"},
+    	{BiometricEvaluation::Feature::Sort::Kind::PolarCOIAscending,
+	    "Polar Center of Image Ascending"},
+	{BiometricEvaluation::Feature::Sort::Kind::PolarCOIDescending,
+	    "Angle Center of Image Descending"}
+};
