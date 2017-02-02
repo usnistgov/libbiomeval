562e7486ccb77ffeedec378bdbcc27b33cab66ed
Wayne Salamon <wsalamon@nist.gov>
2017-02-02 11:47:21 -0500

    Add a test of accessing a shared memory counter that is mediated by a Semaphore object. Run the test many times.
    [git-p4: depot-paths = "//depot/projects/EVALUATION/Main/common/": change = 8235]

--- src/test/test_be_process_semaphore.cpp
+++ src/test/test_be_process_semaphore.cpp
@@ -8,6 +8,7 @@
  * about its quality, reliability, or any other characteristic.
  */
 
+#include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -24,6 +25,10 @@ namespace BE = BiometricEvaluation;
 
 const std::string semName("/sem1");
 
+static int shmID = 0;
+static int shmCount = 10;
+static uint32_t *shmPtr;
+
 static void
 pMsg(const std::string msg)
 {
@@ -33,9 +38,8 @@ pMsg(const std::string msg)
 int
 childFunction()
 {
-	/* Open the semaphore */
 	std::unique_ptr<BE::Process::Semaphore> sem;
-	pMsg("Open semaphore: ");
+	pMsg("Open existing semaphore: ");
 	try {
 		sem.reset(new
 		    BE::Process::Semaphore(semName));
@@ -98,13 +102,152 @@ childFunction()
 	return (EXIT_SUCCESS);
 }
 
+
+/*
+ * Test a binary Semaphore to control access to a shared memory value.
+ */
 int
-main(int argc, char *argv[])
+childCounter(BE::Process::Semaphore *sem)
+{
+	/* Shared memory testing with mediation by a Semaphore */
+	for (int i = 0; i < shmCount; i++) {
+		sem->wait(false);
+		uint32_t val = *shmPtr;
+		val++;
+		*shmPtr = val;
+		sem->post();
+		usleep(100);
+	}
+	if (shmdt(shmPtr) != 0) {
+		std::cerr << "Failed to detach from shared mem.\n";
+	}
+	return 0;
+}
+
+int
+parentCounter()
 {
 	/* Create a Semaphore */
 	std::unique_ptr<BE::Process::Semaphore> sem;
+	try {
+		sem.reset(new
+			BE::Process::Semaphore(semName, S_IRUSR|S_IWUSR, 1));
+	} catch (BE::Error::Exception &e) {
+		pMsg("parentCounter() could not create Semaphore.\n");
+		return (EXIT_FAILURE);
+	}
+
+	/*
+	 * Create a child process and have it increment a shared memory
+	 * value after grabbing the Semaphore.
+	 */
+	/* Get a shared memory segment */
+	shmID = shmget(IPC_PRIVATE, sizeof(uint32_t), 0600);
+	if (shmID <= 0) {
+		pMsg("Could not get shared memory: " +
+		    BE::Error::errorStr() + ".\n");
+		return (EXIT_FAILURE);
+	}
+	shmPtr = (uint32_t *)shmat(shmID, 0, 0);
+	if (shmPtr == (void *)-1) {
+		std::cout << "Could not attach to shared memory.\n";
+		return (EXIT_FAILURE);
+	}
+	struct shmid_ds buf;
+	shmctl(shmID, IPC_STAT, &buf);
+
+	int retVal = EXIT_SUCCESS;
+	pid_t pid = fork();
+	switch (pid) {
+	case 0:			/* child */
+	{
+		int rc = childCounter(sem.get());
+		std::exit(rc);
+		break;
+	}
+	case -1:		/* error */
+		pMsg("Error on fork: " + BE::Error::errorStr());
+		std::cout << std::endl;
+		return (EXIT_FAILURE);
+		break;
+	default:		/* parent */
+		shmctl(shmID, IPC_STAT, &buf);
+		for (int i = 0; i < shmCount; i++) {
+			sem->wait(false);
+			uint32_t val = *shmPtr;
+			val++;
+			*shmPtr = val;
+			sem->post();
+			usleep(125);
+		}
+		/* Wait for other detachments from the shared mem segment */
+		struct shmid_ds buf;
+		while (true) {
+			shmctl(shmID, IPC_STAT, &buf);
+			if (buf.shm_nattch == 1) {
+				break;
+			}
+			pMsg("Waiting for shared memory detachments; ");
+			std::cout << "Current count is "<< buf.shm_nattch
+			    << std::endl;
+			sleep(1);
+		}
+		if (*shmPtr != shmCount * 2) {
+			pMsg("Shared memory counter has value ");
+			std::cout << *shmPtr << ", should be "
+			    << (shmCount * 2) << ".\n";
+			retVal = EXIT_FAILURE;
+		}
+		if (shmdt(shmPtr) != 0) {
+			std::cerr << "Failed to detach from shared mem.\n";
+			retVal = EXIT_FAILURE;
+		}
+		if (shmctl(shmID, IPC_RMID, 0) != 0) {
+			std::cerr << "Failed to remove shared mem.\n";
+			retVal = EXIT_FAILURE;
+		}
+	}
+	return (retVal);
+}
+
+int
+main(int argc, char *argv[])
+{
+	bool success = false;
+	std::unique_ptr<BE::Process::Semaphore> sem;
+
+	/* Open a bogus semaphore */
+	pMsg("Open bogus semaphore: ");
+	try {
+		sem.reset(new
+		    BE::Process::Semaphore("/fefifofum"));
+		std::cout << "Failure." << std::endl;
+	} catch (BE::Error::ObjectDoesNotExist &e) {
+		std::cout << "Success." << std::endl;
+		success = true;
+	} catch (BE::Error::Exception &e) {
+		std::cout << "Failed: " << e.whatString() << std::endl;
+	}
+	if (!success) {
+		std::cout << "Failed. " << std::endl;
+		return (EXIT_FAILURE);
+	}
+
+	/* Create a Semaphore */
 	pMsg("Create semaphore: ");
 	try {
+		sem.reset(new
+		    BE::Process::Semaphore(semName, S_IRUSR|S_IWUSR, 0));
+		std::cout << "Success." << std::endl;
+	} catch (BE::Error::ObjectExists &e) {
+		/* Remove a stale semaphore */
+		pMsg("Removing failed Semaphore: ");
+		// XXX replace with class function if ever implemented
+		if (sem_unlink(semName.c_str()) != 0) {
+			std::cout << "Failed: " 
+			    << BE::Error::errorStr() << ".\n";
+			return (EXIT_FAILURE);
+		}
 		sem.reset(new
 		    BE::Process::Semaphore(semName, S_IRUSR|S_IWUSR, 0));
 		std::cout << "Success." << std::endl;
@@ -113,18 +256,42 @@ main(int argc, char *argv[])
 		return (EXIT_FAILURE);
 	}
 
+	pMsg("Attempt to create an existing semaphore: ");
+	success = false;
+	try {
+		sem.reset(new
+		    BE::Process::Semaphore(semName, S_IRUSR|S_IWUSR, 0));
+	} catch (BE::Error::ObjectExists &e) {
+		std::cout << "Success." << std::endl;
+		success = true;
+	} catch (BE::Error::Exception &e) {
+		std::cout << "Failed: " << e.whatString() << std::endl;
+		return (EXIT_FAILURE);
+	}
+	if (!success) {
+		std::cout << "Failed. " << std::endl;
+		return (EXIT_FAILURE);
+	}
+
 	/* Create a child process and have it wait on the semaphore */
 	pid_t pid = fork();
 	switch (pid) {
 	case 0:			/* child */
+	{
+		/*
+		 * Close the Semaphore so we can re-open it by name in
+		 * the child function.
+		 */
 		pMsg("Closing semaphore: ");
 		sem.reset(NULL);
 		std::cout << "Success." << std::endl;
-		return (childFunction());
+		int rc = childFunction();
+		pMsg("Exiting.\n");
+		std::exit(rc);
 		break;
+	}
 	case -1:		/* error */
-		pMsg("Error on fork: " + BE::Error::errorStr());
-		std::cout << std::endl;
+		pMsg("Error on fork: " + BE::Error::errorStr() + "\n");
 		return (EXIT_FAILURE);
 		break;
 	default:		/* parent */
@@ -154,7 +321,20 @@ main(int argc, char *argv[])
 		}
 		sleep(2);
 		break;
-
 	}
-	return (EXIT_SUCCESS);
+	/*
+	 * Test that shared access to a counter is consistent, many times.
+	 */
+	sem.reset(NULL);
+	int count = 50;
+	pMsg("Testing shared counter mediation over ");
+	std::cout << count << " iterations.\n" << std::flush;
+	for (int i = 0; i < count; i++) {
+		if (parentCounter() != EXIT_SUCCESS) {
+			pMsg("Failed on test number "); std::cout << i << ".\n";
+			return (EXIT_FAILURE);
+		}
+	}
+	pMsg("Success.\n");
+	return (EXIT_FAILURE);
 }
