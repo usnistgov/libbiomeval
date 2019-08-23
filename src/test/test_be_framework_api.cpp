usnistgov/libbiomeval

commit db223454794e01c10fa6550836f349e428914154

Change Framework::Status for Image callbacks.
Framework::Status had been a ready-made object for evaluation test drivers to use for returning from API methods. It ends up not being used much (at all?) because we are rarely using libbiomeval in evaluation APIs. Instead, repurpose this name for a container to store information libbiomeval operations. Make use of this as the parameter for Image:: callbacks.

src/include/be_framework_status.h
@@ -14,18 +14,34 @@
 #include <ostream>
 #include <string>
 
+#include <be_framework_enumeration.h>
+
 namespace BiometricEvaluation
 {
 	namespace Framework
 	{
-		/** Type to be returned from API methods */
+		/** Information communicated back from framework methods. */
 		class Status
 		{
 		public:
-			/** Successful return. Nothing to report. */
-			static const int32_t OK = 0;
+			/** Type of status received. */
+			enum class Type
+			{
+				/**
+				 * Informational/debugging. Processing should
+				 * continue.
+				 */
+				Debug,
+				/**
+				 * Something seems off about the operation, but
+				 * the output might be fine.
+				 */
+				Warning,
+				/** Processing absolutely should stop. */
+				Error
+			};
 
-			/** 
+			/**
 			 * @brief
 			 * Status constructor.
 			 *
@@ -35,23 +51,23 @@ namespace BiometricEvaluation
 			 * Message providing insight into code's value.
 			 */
 			Status(
-			    int32_t code = OK,
-			    const std::string &message = "")
-			    noexcept;
+			    Type type,
+			    const std::string &message,
+			    const std::string &identifier = "");
 
 			/**
 			 * @brief
-			 * Obtain the return code from this Status
+			 * Obtain the Type of this Status' message.
 			 *
-			 * @return 
-			 * Return code
+			 * @return
+			 * Type of status
 			 */
-			inline int32_t
-			getCode()
+			inline Type
+			getType()
 			    const
 			    noexcept
 			{
-				return (this->_code);
+				return (this->_type);
 			}
 
 			/**
@@ -59,7 +75,7 @@ namespace BiometricEvaluation
 			 * Obtain the explanatory message from this Status.
 			 *
 			 * @return
-			 * Explanator message.
+			 * Explanatory message.
 			 *
 			 * @note
 			 * May be empty.
@@ -72,11 +88,34 @@ namespace BiometricEvaluation
 				return (this->_message);
 			}
 
+			/**
+			 * @brief
+			 * Obtain the identifier from this Status.
+			 * @details
+			 * The identifier is used to provide more context about
+			 * the message and is user-defined.
+			 *
+			 * @return
+			 * Identifier associated with this Status.
+			 *
+			 * @note
+			 * May be empty.
+			 */
+			inline std::string
+			getIdentifier()
+			    const
+			    noexcept
+			{
+				return (this->_identifier);
+			}
+
 		private:
-			/** Return code */
-			int32_t _code;
-			/** Explanatory message (optional) */
-			std::string _message;
+			/** Type of Status */
+			Type _type{Type::Debug};
+			/** Explanatory message */
+			std::string _message{};
+			/** Identifier */
+			std::string _identifier{};
 		};
 
 		/**
@@ -85,7 +124,7 @@ namespace BiometricEvaluation
 		 *
 		 * @param status
 		 * Status object to convert.
-		 * 
+		 *
 		 * @return
 		 * Textual representation of status.
 		 */
@@ -111,5 +150,9 @@ namespace BiometricEvaluation
 		    const Status &status);
 	}
 }
+
+BE_FRAMEWORK_ENUMERATION_DECLARATIONS(
+    BiometricEvaluation::Framework::Status::Type,
+    BE_Framework_Status_Type_EnumToStringMap);
 
 #endif /* BE_FRAMEWORK_STATUS_H_ */

src/include/be_image_image.h
@@ -16,6 +16,7 @@
 #include <stdexcept>
 #include <memory>
 
+#include <be_framework_status.h>
 #include <be_io.h>
 #include <be_image.h>
 #include <be_memory_autoarray.h>
@@ -49,9 +50,7 @@ namespace BiometricEvaluation
 		class Image {
 		public:
 			using statusCallback_t = std::function<void(
-			    const std::string &,
-			    const std::string &,
-			    const IO::StatusType)>;
+			    const Framework::Status)>;
 
 			/**
 		 	 * @brief
@@ -519,27 +518,20 @@ namespace BiometricEvaluation
 			 * Default handling of statuses sent from image
 			 * processing libraries.
 			 *
-			 * @param identifier
-			 * Identifier of the image being processed when the
-			 * status was sent.
 			 * @param status
 			 * Status received.
-			 * @param statusType
-			 * The type of status received.
 			 *
 			 * @throw Error::StrategyError
-			 * statusType == StatusType::Error
+			 * status.type == Framework::Status::Type::Error
 			 *
 			 * @note
 			 * Custom implementations of signature statusCallback_t
-			 * should throw an exception when statusType ==
-			 * StatusType::Error.
+			 * should throw an exception when status.type ==
+			 * Framework::Status::Type::Error.
 			 */
 			static void
 			defaultStatusCallback(
-			    const std::string &identifier,
-			    const std::string &status,
-			    const IO::StatusType statusType);
+			    const Framework::Status &status);
 
 		protected:
 			/**

src/include/be_image_jpeg.h
@@ -110,7 +110,7 @@ namespace BiometricEvaluation
 			static void
 			callStatusCallback(
 			    const j_common_ptr cinfo,
-			    const IO::StatusType statusType);
+			    const Framework::Status::Type statusType);
 
 			/**
 			 * @brief

src/include/be_io.h
@@ -47,31 +47,11 @@ namespace BiometricEvaluation {
 			 */
 			ReadOnly = 1
 		};
-
-		/** Type of status received. */
-		enum class StatusType
-		{
-			/**
-			 * Informational/debugging. Processing should
-			 * continue.
-			 */
-			Debug,
-			/**
-			 * Something seems off about the operation, but the
-			 * output might be fine.
-			 */
-			Warning,
-			/** Processing absolutely should stop. */
-			Error
-		};
 	}
 }
 
 BE_FRAMEWORK_ENUMERATION_DECLARATIONS(
     BiometricEvaluation::IO::Mode,
     BE_IO_Mode_EnumToStringMap);
-BE_FRAMEWORK_ENUMERATION_DECLARATIONS(
-    BiometricEvaluation::IO::StatusType,
-    BE_IO_StatusType_EnumToStringMap);
 
 #endif	/* __BE_IO_H__ */

src/libbiomeval/be_framework_status.cpp
@@ -8,14 +8,20 @@
  * about its quality, reliability, or any other characteristic.
  */
 
+#include <algorithm>
+#include <cctype>
+
 #include <be_framework_status.h>
 
+using namespace BiometricEvaluation::Framework::Enumeration;
+
 BiometricEvaluation::Framework::Status::Status(
-    int32_t code,
-    const std::string &message)
-    noexcept :
-    _code(code),
-    _message(message)
+    Type type,
+    const std::string &message,
+    const std::string &identifier) :
+    _type(type),
+    _message(message),
+    _identifier(identifier)
 {
 
 }
@@ -24,11 +30,19 @@ std::string
 BiometricEvaluation::Framework::to_string(
     const Status &status)
 {
-	std::string s{std::to_string(status.getCode())};
+	std::string prefix{::to_string(status.getType())};
+	std::for_each(prefix.begin(), prefix.end(), [](char &c) {
+	    c = std::toupper(c);
+	});
+	std::string s{'[' + prefix + ']'};
 
 	const auto message = status.getMessage();
 	if (!message.empty())
-		s += " (" + message + ")";
+		s += ' ' + message;
+
+	const auto identifier = status.getIdentifier();
+	if (!identifier.empty())
+		s += " (" + identifier + ')';
 
 	return (s);
 }
@@ -40,3 +54,13 @@ BiometricEvaluation::Framework::operator<<(
 {
 	return (s << to_string(status));
 }
+
+const std::map<BiometricEvaluation::Framework::Status::Type, std::string>
+BE_Framework_Status_Type_EnumToStringMap = {
+    {BiometricEvaluation::Framework::Status::Type::Debug, "Debug"},
+    {BiometricEvaluation::Framework::Status::Type::Warning, "Warning"},
+    {BiometricEvaluation::Framework::Status::Type::Error, "Error"}
+};
+BE_FRAMEWORK_ENUMERATION_DEFINITIONS(
+    BiometricEvaluation::Framework::Status::Type,
+    BE_Framework_Status_Type_EnumToStringMap);

src/libbiomeval/be_image_image.cpp
@@ -374,18 +374,12 @@ BiometricEvaluation::Image::Image::openImage(
 
 void
 BiometricEvaluation::Image::Image::defaultStatusCallback(
-    const std::string &identifier,
-    const std::string &status,
-    const IO::StatusType statusType)
+    const BiometricEvaluation::Framework::Status &status)
 {
-	if (statusType != IO::StatusType::Error)
+	if (status.getType() != Framework::Status::Type::Error)
 		return;
 
-	if (identifier.empty())
-		throw BE::Error::StrategyError(status);
-	else
-		throw BE::Error::StrategyError(status + " (Identifier = " +
-		    identifier + ')');
+	throw BE::Error::StrategyError(to_string(status));
 }
 
 BiometricEvaluation::Image::Image::statusCallback_t

src/libbiomeval/be_image_jpeg.cpp
@@ -304,7 +304,7 @@ BiometricEvaluation::Image::JPEG::isJPEG(
 void
 BiometricEvaluation::Image::JPEG::callStatusCallback(
     const j_common_ptr cinfo,
-    const BiometricEvaluation::IO::StatusType statusType)
+    const BiometricEvaluation::Framework::Status::Type statusType)
 {
 	if (cinfo->client_data == nullptr)
 		return;
@@ -313,14 +313,14 @@ BiometricEvaluation::Image::JPEG::callStatusCallback(
 	cinfo->err->format_message(cinfo, buffer);
 
 	const JPEG *jpeg = static_cast<JPEG*>(cinfo->client_data);
-	jpeg->getStatusCallback()(jpeg->getIdentifier(), buffer, statusType);
+	jpeg->getStatusCallback()({statusType, buffer, jpeg->getIdentifier()});
 }
 
 void
 BiometricEvaluation::Image::JPEG::error_exit(
     j_common_ptr cinfo)
 {
-	JPEG::callStatusCallback(cinfo, IO::StatusType::Error);
+	JPEG::callStatusCallback(cinfo, Framework::Status::Type::Error);
 
 	/*
 	 * libjpeg must stop at this point, so if the message handler didn't
@@ -346,13 +346,13 @@ BiometricEvaluation::Image::JPEG::emit_message(
     j_common_ptr cinfo,
     int msg_level)
 {
-	IO::StatusType level{};
+	Framework::Status::Type level{};
 	if (msg_level < 0)
-		level = IO::StatusType::Warning;
+		level = Framework::Status::Type::Warning;
 	else
-		level = IO::StatusType::Debug;
+		level = Framework::Status::Type::Debug;
 
-	if (level == IO::StatusType::Warning) {
+	if (level == Framework::Status::Type::Warning) {
 		/* Only print first warning unless high trace level set */
 		if ((cinfo->err->num_warnings == 0) ||
 		    (cinfo->err->trace_level >= 3))

src/libbiomeval/be_image_jpeg2000.cpp
@@ -212,8 +212,8 @@ BiometricEvaluation::Image::JPEG2000::openjpeg_error(
 {
 	if (client_data != nullptr) {
 		const JPEG2000 *jp2 = static_cast<const JPEG2000*>(client_data);
-		jp2->getStatusCallback()(jp2->getIdentifier(), msg,
-		    IO::StatusType::Error);
+		jp2->getStatusCallback()({Framework::Status::Type::Error,
+		    msg, jp2->getIdentifier()});
 	}
 
 	/* We can't continue on errors, so if handler won't throw, we will. */
@@ -229,8 +229,8 @@ BiometricEvaluation::Image::JPEG2000::openjpeg_warning(
 		return;
 
 	const JPEG2000 *jp2 = static_cast<const JPEG2000*>(client_data);
-	jp2->getStatusCallback()(jp2->getIdentifier(), msg,
-	    IO::StatusType::Warning);
+	jp2->getStatusCallback()({Framework::Status::Type::Warning, msg,
+	    jp2->getIdentifier()});
 }
 
 void
@@ -242,8 +242,8 @@ BiometricEvaluation::Image::JPEG2000::openjpeg_info(
 		return;
 
 	const JPEG2000 *jp2 = static_cast<const JPEG2000*>(client_data);
-	jp2->getStatusCallback()(jp2->getIdentifier(), msg,
-	    IO::StatusType::Debug);
+	jp2->getStatusCallback()({Framework::Status::Type::Debug, msg,
+	    jp2->getIdentifier()});
 }
 
 bool

commit 3cefa7922564f4225eedbe42adcc716c8839a60d

Remove Status from Framework::API test program.
Regression since db223454794e01c10fa6550836f349e428914154.

src/test/test_be_framework_api.cpp
@@ -10,7 +10,6 @@
 
 #include <be_framework_api.h>
 #include <be_framework_enumeration.h>
-#include <be_framework_status.h>
 
 #include <iostream>
 
@@ -22,13 +21,13 @@ namespace Eval
 	/*
 	 * Custom Status object
 	 */
-	class Status : public BE::Framework::Status
+	class Status
 	{
 	public:
 		/** Use enumerations to hide away integer return codes. */
 		enum class Code
 		{
-			Success = BE::Framework::Status::OK,
+			Success = 0,
 			BadImage,
 			BadTemplate,
 			VendorDefined
@@ -44,13 +43,31 @@ namespace Eval
 		 */
 		Status(
 		    const Status::Code &code = Code::Success,
-		    const std::string &message = "")
-		    noexcept;
+		    const std::string &message = "");
 
-		/** Convert code stored in parent to the eval-specific enum.*/
+		/** @return Status code returned from a method. */
 		Code
-		getEvalStatusCode()
-		    const;
+		getCode()
+		    const
+		    noexcept
+		{
+			return (this->code);
+		}
+
+		/** @return Information regarding the return from a method. */
+		std::string
+		getMessage()
+		    const
+		    noexcept
+		{
+			return (this->message);
+		}
+
+	private:
+		/** Status code returned a method. */
+		Code code{};
+		/** Information regarding the return from the method. */
+		std::string message{};
 	};
 
 /******************************************************************************
@@ -134,24 +151,18 @@ BE_FRAMEWORK_ENUMERATION_DEFINITIONS(
 
 Eval::Status::Status(
     const Status::Code &code,
-    const std::string &message)
-    noexcept :
-    BE::Framework::Status::Status(to_int_type(code), message) {}
-
-Eval::Status::Code
-Eval::Status::getEvalStatusCode()
-    const
+    const std::string &message) :
+    code(code),
+    message(message)
 {
-	const auto code = this->getCode();
-	return (to_enum<Code>(code));
+
 }
 
 std::string
 Eval::to_string(
     const Eval::Status &status)
 {
-	std::string s{BE::Framework::Enumeration::
-	    to_string(status.getEvalStatusCode())};
+	std::string s{BE::Framework::Enumeration::to_string(status.getCode())};
 
 	const auto message = status.getMessage();
 	if (!message.empty())
@@ -170,10 +181,11 @@ main()
 	for (uint8_t i = 0; i < 10; ++i) {
 		const auto status = Eval::createTemplate(i);
 		std::cout << "Returned: " << status << std::endl;
-		std::cout << "\tCode: " << status.getCode() << std::endl;
-		std::cout << "\tEval Code: " <<
-		    to_string(status.getEvalStatusCode()) << std::endl;
-		std::cout << "\tMessage: " << status.getMessage() << std::endl;
+		std::cout << "\tCode: " << to_string(status.getCode()) <<
+		    " (" << to_int_type(status.getCode()) << ")\n";
+		const auto message = status.getMessage();
+		std::cout << "\tMessage: " <<
+		    (message.empty() ? "<NO MESSAGE>" : message) << '\n';
 	}
 
 	BE::Framework::API<Eval::Status> API1;