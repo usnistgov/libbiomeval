cd01503790475608502730de371479959b56a71f
Greg Fiumara <gregory.fiumara@nist.gov>
2020-06-22 12:33:19 -0400

    CMake 2 does not understand this commenting.

--- src/libbiomeval/cmake/Modules/FindSQLite3.cmake
+++ src/libbiomeval/cmake/Modules/FindSQLite3.cmake
@@ -1,33 +1,33 @@
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file Copyright.txt or https://cmake.org/licensing for details.
-
+#
 #[=======================================================================[.rst:
-FindSQLite3
------------
-
-Find the SQLite libraries, v3
-
-IMPORTED targets
-^^^^^^^^^^^^^^^^
-
-This module defines the following :prop_tgt:`IMPORTED` target:
-
-``SQLite::SQLite3``
-
-Result variables
-^^^^^^^^^^^^^^^^
-
-This module will set the following variables if found:
-
-``SQLite3_INCLUDE_DIRS``
-  where to find sqlite3.h, etc.
-``SQLite3_LIBRARIES``
-  the libraries to link against to use SQLite3.
-``SQLite3_VERSION``
-  version of the SQLite3 library found
-``SQLite3_FOUND``
-  TRUE if found
-
+#FindSQLite3
+#-----------
+#
+#Find the SQLite libraries, v3
+#
+#IMPORTED targets
+#^^^^^^^^^^^^^^^^
+#
+#This module defines the following :prop_tgt:`IMPORTED` target:
+#
+#``SQLite::SQLite3``
+#
+#Result variables
+#^^^^^^^^^^^^^^^^
+#
+#This module will set the following variables if found:
+#
+#``SQLite3_INCLUDE_DIRS``
+#  where to find sqlite3.h, etc.
+#``SQLite3_LIBRARIES``
+#  the libraries to link against to use SQLite3.
+#``SQLite3_VERSION``
+#  version of the SQLite3 library found
+#``SQLite3_FOUND``
+#  TRUE if found
+#
 #]=======================================================================]
 if(${CMAKE_VERSION} VERSION_LESS "3.14")
 # Look for the necessary header
