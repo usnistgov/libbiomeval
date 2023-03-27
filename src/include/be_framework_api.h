usnistgov/libbiomeval

commit c127eda39a71411af52c93f3a2e73a59560a519e

Enable wholesale query/change of API protections

src/include/be_framework_api.h
@@ -248,6 +248,66 @@ namespace BiometricEvaluation
 
 			/**
 			 * @brief
+			 * Obtain whether or not **all** protections enabled by
+			 * this object are enabled.
+			 * @details
+			 * Protections include:
+			 *   * Catching exceptions
+			 *   * Not rethrowing exceptions
+			 *   * Enabling WatchdogTimer
+			 *   * Enabling SignalManager
+			 *
+			 * @return
+			 * `true` if **all** protections are enabled, `false` if
+			 * one or more protections are disabled.
+			 *
+			 * @note
+			 * Individual protection statuses may be queried through
+			 * their respective objects/methods.
+			 */
+			bool
+			protectionsEnabled()
+			    const
+			{
+				return (this->willCatchExceptions() &&
+				    !this->willRethrowExceptions() &&
+				    this->getWatchdog()->isEnabled() &&
+				    this->getSignalManager->isEnabled());
+			}
+
+			/**
+			 * @brief
+			 * Wholesale change of process protections enabled by
+			 * this object.
+			 * @details
+			 * Protections include:
+			 *   * Catching exceptions
+			 *   * Not rethrowing exceptions
+			 *   * Enabling WatchdogTimer
+			 *   * Enabling SignalManager
+			 *
+			 * @param protectionsEnabled
+			 * `true` if **all** protections should be enabled,
+			 * `false` if **all** protections should be disabled.
+			 *
+			 * @note
+			 * Protections can be enabled or disabled individually
+			 * through their respective objects/methods.
+			 */
+			void
+			setProtectionsEnabled(
+			    const bool protectionsEnabled)
+			{
+				this->setCatchExceptions(protectionsEnabled);
+				this->setRethrowExceptions(!protectionsEnabled);
+				this->getWatchdog()->setEnabled(
+				    protectionsEnabled);
+				this->getSignalManager->setEnabled(
+				    protectionsEnabled);
+			}
+
+			/**
+			 * @brief
 			 * Obtain whether or not exceptions caught in call()
 			 * will be rethrown.
 			 *

commit 43a59a95819dda5db285c8db2e3ca8d12a0c7377

Missing function call
Regression since c127eda3

src/include/be_framework_api.h
@@ -290,7 +290,7 @@ namespace BiometricEvaluation
 				return (this->willCatchExceptions() &&
 				    !this->willRethrowExceptions() &&
 				    this->getWatchdog()->isEnabled() &&
-				    this->getSignalManager->isEnabled());
+				    this->getSignalManager()->isEnabled());
 			}
 
 			/**
@@ -320,7 +320,7 @@ namespace BiometricEvaluation
 				this->setRethrowExceptions(!protectionsEnabled);
 				this->getWatchdog()->setEnabled(
 				    protectionsEnabled);
-				this->getSignalManager->setEnabled(
+				this->getSignalManager()->setEnabled(
 				    protectionsEnabled);
 			}
 